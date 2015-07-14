2e590cc5f83ede2659efcc143866802d90263da7
nbriz <nickbriz@gmail.com>
2015-07-14 10:16:36 -0500

    changed to port 80

--- server.js
+++ server.js
@@ -267,8 +267,8 @@ app.get('/joselyn', function(req, res) {
 
 // server -------------------------------------------------------------------------
 
-// var server = app.listen(80, function () {
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
+// var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('listening at http://%s:%s', host, port);
